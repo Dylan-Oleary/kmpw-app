2bcba6a5946ac8d1f1952a27cc77001b56428d30
Dylan O'Leary <dylanolearydev@gmail.com>
2022-08-04 15:48:02 -0400

    feat: Make ages on dog cards more specific

--- src/lib/dogs.ts
+++ src/lib/dogs.ts
@@ -1,9 +1,20 @@
 import dayjs from "dayjs";
 
-export const getDogAge: (birthday?: Date) => string | undefined = (birthday) => {
+export const getDogAge: (
+    birthday?: Date,
+    diffFormat?: "days" | "months" | "years"
+) => string | undefined = (birthday, diffFormat = "years") => {
     if (!birthday) return undefined;
 
-    const age = dayjs().diff(birthday, "years");
+    const age = dayjs().diff(birthday, diffFormat);
 
-    return `${age * 7} (${age} Human)`;
+    switch (diffFormat) {
+        case "days":
+            return `${age} day${age === 1 ? "" : "s"}`;
+        case "months":
+            return age > 0 ? `${age} month${age > 1 ? "s" : ""}` : getDogAge(birthday, "days");
+        case "years":
+        default:
+            return age > 0 ? `${age * 7} (${age} Human)` : getDogAge(birthday, "months");
+    }
 };
